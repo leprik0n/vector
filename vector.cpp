20c79b07fdd9df0ac02ee23dd96f13d32c45d517
leprik0n <diablo_soft@mail.ru>
2016-10-09 00:47:36 +0400

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,157 +1,133 @@
 #include <iostream>
+#include <stdexcept>
+#include <limits>
 #include "vector.h"
 #include <cmath>
-#include <limits>
 using namespace std;
 
-Vector::Vector()
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]=0;
+Vector::Vector(){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] = 0;
     }
 }
 
-Vector::Vector(double k)
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]=k;
-     }   
+Vector::Vector(double k){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] = k;
+    }
 }
 
-Vector::Vector(const Vector& p)
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]=p.vec[i];
-     }  
+Vector::Vector(const Vector& p){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] = p.vec[i];
+    }
 }
 
-Vector& Vector::operator=(const Vector& p)
-{
-     if(this!=&p){
-         for(unsigned int i = 0; i < n; i++){
-             vec[i]=p.vec[i];
-         }
-     }    
-     return *this;
-}
-
-double Vector::operator[](unsigned long i) const
-{
-     if(i > (n-1)){ 
-        throw "out of range";
-     }   
-     return vec[i];
-}
-
-double& Vector::operator[](unsigned long i)
-{
-     if(i > (n-1)){ 
-         throw "out_of range";
-     }   
-     return vec[i];
-}
-
-Vector& Vector::operator+=(const Vector& p)
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]+=p.vec[i];
-     }    
-     return *this;
-}
-
-Vector& Vector::operator-=(const Vector& p)
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]-=p.vec[i];
-     }    
-     return *this;
-}
-
-Vector& Vector::operator*=(double k)
-{
-     for(unsigned int i = 0; i < n; i++){
-         vec[i]*=k;
-     }    
-     return *this;
-}
-
-Vector& Vector::operator/=(double k)
-{
-     if(k){
-         for(unsigned int i = 0; i < n; i++){
-            vec[i]/=k;
-        }
-     }
-     
-     else{ 
-         throw "division by zero";
-     }   
+Vector& Vector::operator=(const Vector& p){
+    if(this != &p){
+            for(unsigned int i = 0; i < n; i++){
+                    vec[i] = p.vec[i];
+            }
+    }
     return *this;
 }
 
-bool operator==(const Vector& p1, const Vector& p2)
-{
-     for(unsigned int i = 0; i < p1.n; i++){ 
-         if(abs(p1.vec[i]-p2.vec[i])>numeric_limits<double>::epsilon())
-            return false;
-     }        
-     return true;
+double Vector::operator[](unsigned long i) const{
+    if(i > (n-1)){
+            throw out_of_range("out of range");
+    }
+    return vec[i];
 }
 
-ostream& operator<<(ostream& os,const Vector& p)
-{
-     for(unsigned int i = 0; i < p.n; i++){
-         os<<p.vec[i]<<' ';
-     }   
-     return os;
+double& Vector::operator[](unsigned long i){
+    if(i > (n-1)){
+            throw out_of_range("out of range");
+    }
+    return vec[i];
 }
 
-Vector operator+(const Vector& p1, const Vector& p2)
-{
-     return Vector(p1)+=p2;
+Vector& Vector::operator+=(const Vector& p){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] += p.vec[i];
+    }
+    return *this;
 }
 
-Vector operator-(const Vector& p1, const Vector& p2)
-{
-     return Vector(p1)-=p2;
+Vector& Vector::operator-=(const Vector& p){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] -= p.vec[i];
+    }
+    return *this;
 }
 
-Vector operator*(const Vector& p, double k)
-{
-     return Vector(p)*=k;
+Vector& Vector::operator*=(double k){
+    for(unsigned int i = 0; i < n; i++){
+            vec[i] *= k;
+    }
+    return *this;
 }
 
-Vector operator*(double k, const Vector& p)
-{
-     return Vector(p)*=k;
+Vector& Vector::operator/=(double k){
+    if(k){
+            for(unsigned int i = 0; i < n; i++){
+                    vec[i] /= k;
+            }
+    }
+    else{
+            throw "division by zero not impossible";
+    }
+    return *this;
 }
 
-Vector operator/(const Vector &p, double k)
-{
-     if(k)
-     {
-        return Vector(p)/=k;
-     }
-     else{
-        throw "division by zero";
-     }   
+bool operator==(const Vector& p1, const Vector& p2){
+    for(unsigned int i = 0; i < p1.n; i++){
+            if(abs(p1.vec[i] - p2.vec[i])>numeric_limits<double>::epsilon()){
+                return false;
+            }
+    }
+    return true;
 }
 
-double operator^(const Vector& p1, const Vector& p2)
-{
-     double sum=0;
-     for(unsigned int i = 0;i < p1.n; i++){
-         sum+=p1.vec[i]*p2.vec[i];
-     }    
-     return sum;
+ostream& operator<<(ostream& os,const Vector& p){
+    for(unsigned int i = 0; i < p.n; i++){
+            os<<p.vec[i]<<' ';
+    }
+    return os;
 }
 
-bool operator!=(const Vector& p1, const Vector& p2)
-{
-     return !(p1==p2);
+Vector operator+(const Vector& p1, const Vector& p2){
+    return Vector(p1) += p2;
 }
 
-Vector Vector::operator-()const
-{
-     return Vector(*this)*=-1;
-} 
+Vector operator-(const Vector& p1, const Vector& p2){
+    return Vector(p1) -= p2;
+}
+
+Vector operator*(const Vector& p, double k){
+    return Vector(p) *= k;
+}
+
+Vector operator*(double k, const Vector& p){
+    return Vector(p) *= k;
+}
+
+Vector operator/(const Vector& p, double k){
+    if(k){
+            return Vector(p) /= k;
+    }
+    else{
+        throw "division by zero not impossible";
+    }
+}
+
+double operator^(const Vector& p1, const Vector& p2){
+    double sum = 0;
+    for(unsigned int i = 0; i < p1.n; i++){
+            sum += p1.vec[i]*p2.vec[i];
+    }
+    return sum;
+}
+
+bool operator!=(const Vector& p1, const Vector& p2){
+    return !(p1 == p2);
+}
