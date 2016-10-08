491ea837169d5f0d2b0617e2904fe459014557ea
leprik0n <diablo_soft@mail.ru>
2016-10-09 00:48:20 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,67 +1,196 @@
 #include <iostream>
+#include <stdexcept>
 #include "vector.h"
+#include <assert.h>
 using namespace std;
+
+void ctor_novalue(){
+    Vector buf{};
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == 0);
+    }
+    cout<<"1_Passed"<<endl;
+}
+
+void ctor_value(){
+    Vector buf{5};
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == 5);
+    }
+    cout<<"2_Passed"<<endl;
+}
+
+void ctor_copy(){
+    Vector buf;
+    Vector bbf{buf};
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bbf[i] == buf[i]);
+    }
+    cout<<"3_Passed"<<endl;
+}
+
+void oper_assign(){
+    Vector buf;
+    Vector bbf = buf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bbf[i] == buf[i]);
+    }
+    cout<<"4_Passed"<<endl;
+}
+
+void oper_idx(){
+    Vector buf{6};
+    buf[2] = 9;
+    assert(buf[2] == 9);
+    cout<<"5_Passed"<<endl;
+}
+
+void oper_plus_equal(){
+    Vector buf{9};
+    Vector bbf{2};
+    buf += bbf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == 11);
+    }
+    cout<<"6_Passed"<<endl;
+}
+
+void oper_minus_equal(){
+    Vector buf{10};
+    Vector bbf{-3};
+    buf -= bbf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == 13);
+    }
+    cout<<"7_Passed"<<endl;
+}
+
+void oper_mult_equal(){
+    Vector buf{2};
+    buf *= 12;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == 24);
+    }
+    cout<<"8_Passed"<<endl;
+}
+
+void oper_div_equal(){
+    Vector buf{2};
+    buf /= -1;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(buf[i] == -2);
+    }
+    cout<<"9_Passed"<<endl;
+}
+
+void oper_equal(){
+    Vector buf{};
+    Vector bbf{buf};
+    buf[0] = 1.020; buf[1] = 2.01; buf[2] = 3.0012;
+    bbf[0] = 1.020; bbf[1] = 2.01; bbf[2] = 3.0312;
+    bool k = (buf == bbf);
+    assert(k == false);
+    cout<<"10_Passed"<<endl;
+}
+
+void oper_plus(){
+    Vector buf{2};
+    Vector bbf{4};
+    Vector bfs = buf+bbf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bfs[i] == 6);
+    }
+    cout<<"11_Passed"<<endl;
+}
+
+void oper_minus(){
+    Vector buf{3};
+    Vector bbf{-1};
+    Vector bfs = buf-bbf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bfs[i] == 4);
+    }
+    cout<<"12_Passed"<<endl;
+}
+
+void oper_mult_right(){
+    Vector buf{2};
+    Vector bbf = buf*4;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bbf[i] == 8);
+    }
+    cout<<"13_Passed"<<endl;
+}
+
+void oper_mult_left(){
+    Vector buf{2};
+    Vector bbf = 4*buf;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bbf[i] == 8);
+    }
+    cout<<"14_Passed"<<endl;
+}
+
+void oper_div(){
+    Vector buf{15};
+    Vector bbf = buf/3;
+    for(unsigned int i = 0; i < 3; i++){
+            assert(bbf[i] == 5);
+    }
+    cout<<"15_Passed"<<endl;
+}
+
+void oper_scalar(){
+    Vector buf{1};
+    Vector bbf{buf};
+    double sum = bbf^buf;
+    assert(sum == 3);
+    cout<<"16_Passed"<<endl;
+}
+
+void oper_not_equal(){
+    Vector buf{};
+    Vector bbf{buf};
+    buf[0] = 1.020; buf[1] = 2.0113; buf[2] = 3.0312;
+    bbf[0] = 1.020; bbf[1] = 2.01; bbf[2] = 3.0312;
+    bool k = (buf != bbf);
+    assert(k == true);
+    cout<<"17_Passed"<<endl;
+}
+
 
 int main()
 {
+
 try
 {
-  Vector buf(1);
-  cout<<"Create V(1)="<<' '<<buf<<endl;
-  cout<<endl;
-  Vector str(5);
-  cout<<"Create V(5)="<<' '<<str<<endl;
-  cout<<endl;
-  buf+=str;
-  cout<<"V(1)+=V(5)"<<' '<<buf<<endl;
-  cout<<endl;
-  buf-=str;
-  cout<<"V(6)-=V(5)"<<' '<<buf<<endl;
-  cout<<endl;
-  buf*=(-2);
-  cout<<"V(1)*=(-2)"<<' '<<buf<<endl;
-  cout<<endl;
-  cout<<"V(-2)="<<' '<<buf[0]<<' '<<buf[1]<<' '<<buf[2]<<' '<<endl;
-  buf/=4;
-  cout<<endl;
-  cout<<"V(-2)/=4"<<' '<<buf<<endl;
-  cout<<endl;
-  Vector p1=str-buf;
-  cout<<"V()=V(5)-V(-0,5)"<<' '<<p1<<endl;
-  Vector p2=p1+str;
-  cout<<endl;
-  cout<<"V()=V(5,5)+V(5)"<<' '<<p2<<endl;
-  cout<<endl;
-  Vector p3=p2*12;
-  cout<<"V()=V(10,5)*12"<<' '<<p3<<endl;
-  cout<<endl;
-  Vector p4=p3/4;
-  cout<<"V()=V(126)/4"<<' '<<p4<<endl;
-  Vector p5=p4;
-  cout<<endl;
-  cout<<"V()=V(31,5)"<<' '<<p5<<endl;
-  double sum=p5^p5;
-  cout<<endl;
-  cout<<"V(31,5)^V(31,5)"<<' '<<sum<<endl;
-  cout<<endl;
-  p1=Vector(0);
-  p2=Vector(0);
-  bool b=false;
-  b=(p1==p2);
-  cout<<"V(0)==V(0)"<<' '<<b<<endl;
-  b=(p1!=p2);
-  cout<<endl;
-  cout<<"V(0)!=V(0)"<<' '<<b<<endl;
+
+ctor_novalue();
+ctor_value();
+ctor_copy();
+oper_assign();
+oper_plus_equal();
+oper_minus_equal();
+oper_mult_equal();
+oper_div_equal();
+oper_equal();
+oper_plus();
+oper_minus();
+oper_mult_right();
+oper_mult_left();
+oper_div();
+oper_scalar();
+oper_not_equal();
+
 }
 
-
-
-catch(const char*exp)
-{
-    cout<<"exception:"<<exp<<endl;
-    cout<<"sorry can't continue!"<<endl;
+catch(const char* op){
+    cout<<"exception:"<<op<<endl;
 }
 
+catch(const std::out_of_range& oop){
+    cout<<"exception:"<<oop.what()<<endl;
+}
 
     return 0;
 }
